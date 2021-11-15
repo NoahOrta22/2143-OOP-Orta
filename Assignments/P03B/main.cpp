d5da8bc13af41881c415b34102d0040b7dc0a411
Noah Orta <50061473+NoahOrta22@users.noreply.github.com>
2021-11-14 22:10:20 -0600

    Create main.cpp

---
+++ Assignments/P03B/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+
+#include "DieRoll.hpp"
+#include "Game.hpp"
+#include "Characters.hpp"
+
+using namespace std;
+
+
+int main() {
+
+  Game game;
+
+  //  Runs the game until there is a winner
+  while(true) {
+    game.FIGHT();
+    
+  }
+  
+} 
